4d01c8b7a1bf96ba2b66947e900b846512416b4b
Pac12Run8000 <gangstarr78992@>
2023-07-13 16:08:13 -0700

    Initial Commit

---
+++ RemoteConfigExample/RemoteConfigExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteConfigExampleApp.swift
+//  RemoteConfigExample
+//
+//  Created by Michelle Grover on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct RemoteConfigExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
